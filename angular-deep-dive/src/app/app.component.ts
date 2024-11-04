c7b43eae2edabe82ef333499319d0c624c9b84f4
Kayla Petersen <petersen1994@gmail.com>
2024-11-04 16:52:07 -0500

    added signals to users

--- angular-deep-dive/src/app/app.component.ts
+++ angular-deep-dive/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummer-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-deep-dive';
+
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log(id);
+  }
+
 }
--- angular-deep-dive/src/app/user/user.component.ts
+++ angular-deep-dive/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from './dummer-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, input, computed, output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +8,31 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser: any = DUMMY_USERS[randomIndex];
+
+  name = input.required<string>();
+  avatar = input.required<string>();
+  id = input.required<string>();
+  select = output<string>();
+
+  imagePath = computed<string>(() => {
+    return 'assets/users/' + this.avatar();
+  })
+
+  onSelectedUser(): void {
+    this.select.emit(this.id())
+  }
+
 }
+
+/*   @Input({required: true}) avatar: string = '';
+  @Input({required: true}) name: string = ''; */
+
+/*   selectedUser = signal<User>(DUMMY_USERS[this.generateRandomIndex()]) ;
+
+  onSelectUser(): void {
+    this.selectedUser.set(DUMMY_USERS[this.generateRandomIndex()]);
+  }
+
+  generateRandomIndex() {
+    return Math.floor(Math.random() * DUMMY_USERS.length);
+  } */
