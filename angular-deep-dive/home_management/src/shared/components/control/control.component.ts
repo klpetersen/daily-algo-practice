94da20471dbd01e4bb9ed3a07e2bf9c000e83897
Kayla Petersen <petersen1994@gmail.com>
2024-11-06 17:12:04 -0500

    updated class in control and css

--- angular-deep-dive/home_management/src/shared/components/control/control.component.ts
+++ angular-deep-dive/home_management/src/shared/components/control/control.component.ts
@@ -1,12 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation, inject, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
+
+  //Gives access to the host element 
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log(this.el)
+  }
+
 }
